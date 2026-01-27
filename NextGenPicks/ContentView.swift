e0d7390d704bc78c7de5f3ec66ff3086060a5c61
Tim Dacalos <2004tim2004@gmail.com>
2026-01-26 16:23:46 -0800

    Added Xcode project

---
+++ NextGenPicks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NextGenPicks
+//
+//  Created by timdac on 1/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NextGenPicks/NextGenPicksApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextGenPicksApp.swift
+//  NextGenPicks
+//
+//  Created by timdac on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct NextGenPicksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
